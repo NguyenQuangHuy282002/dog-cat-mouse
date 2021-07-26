92004229a212571ad81e120e8fcf5704f69e07f3
Huy Nguyen <huynguyen@MacBook-Air-cua-Huy.local>
2021-07-26 10:44:37 +0700

    Let dog eat cat

--- dog.js
+++ dog.js
@@ -1,3 +1,15 @@
-function dog() {
-    
+var chalk = require('chalk');
+
+function Dog(name) {
+    this.name = name;
+    this.stomach = [];
 }
+
+Dog.prototype.eat = function (cat) {
+    this.stomach.push(cat);
+};
+
+Dog.prototype.sayHi = function() {
+    console.log("Hi, i'm " + chalk.red(this.name));
+};
+module.exports = Dog;
